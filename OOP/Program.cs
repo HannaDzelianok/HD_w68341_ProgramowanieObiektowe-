c6c20448b47f5e5b82fbc31e9e8555a1b481f894
Hanna Dzelianok <w68341@student.wsiz.edu.pl>
2024-10-17 11:24:38 +0200

    first commit

---
+++ OOP/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information using System;
+
+using OOP.constractions;
+
+Console.WriteLine("Hello, World!");
+var construction1 = new Construction();
+var construction2 = new Construction(23.0f,45.0f,4,4,"Material");
+Console.WriteLine("hello world");
+
+
+
---
+++ OOP/constructions/Construction.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.constractions
+{
+    internal class Construction
+    {
+
+        public Construction() { }
+        public Construction(float height, float width, int Entrances, int HumanCapacity, string BuildMaterialstring)
+        {
+            
+        }
+        public Construction()
+        {
+            
+        }
+
+        public float Height { get; set; }
+        public float Width { get; set; }
+        public int Entrances  { get; set; }
+        public int HumanCapacity { get; set; }
+        public string BuildMaterial { get; set; }
+
+    }
+}
+//Height typu zmiennoprzecinkowego
+//2. Width typu zmiennoprzecinkowego
+//3. Entrances typu liczba całkowita
+//4. HumanCapacity typu liczba całkowita
+//5. BuildMaterial typu tekstowego
+//Zdefiniuj publiczny konstruktor, który zainicjalizuje poszczególne właściwości.
